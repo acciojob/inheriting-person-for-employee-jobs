d74d650625a5c0ef8c754fcb8946bdf1b6ddcf73
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-03-03 11:28:07 +0530

    boilerplate

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// complete this js code
+function Person(name, age) {}
+
+function Employee(name, age, jobTitle) {}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
